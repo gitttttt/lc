ecbd843c175dfd9b9921f3a6e4b74308dc4cccba
gitttttt <zhangyifan@neotel.com.cn>
2016-10-27 14:55:39 +0800

    again

--- q354.py
+++ q354.py
@@ -1,12 +1,15 @@
 class Solution(object):
     def maxEnvelopes(self, envelopes):
         """
+        http://www.cnblogs.com/grandyang/p/5568818.html
+        not understand, iq zhuoji
         :type envelopes: List[List[int]]
         :rtype: int
         """
         nums = sorted(envelopes,
                       cmp = lambda x,y: x[0] - y[0] if x[0] != y[0] else y[1] - x[1])
         size = len(nums)
+        print nums
         dp = []
         for x in range(size):
             low, high = 0, len(dp) - 1
@@ -25,4 +28,4 @@ class Solution(object):
             print
         return len(dp)
 
-Solution().maxEnvelopes([[1,2], [2,4], [2,3], [3,5]])
+Solution().maxEnvelopes([[5,100],[6,4],[6,3],[6,8]])
