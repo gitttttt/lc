539ecc6381655ec3f01ce485496102f79b26c434
gitttttt <zhangyifan@neotel.com.cn>
2016-11-23 16:02:59 +0800

    segment

--- _test.py
+++ _test.py
@@ -1,4 +1,12 @@
-i1 = 0
-for i in 'abcz':
-    i1 += 1 << (ord(i) - ord('a'))
-print ord('a') - ord('b'), ord('a')
+def gen():
+    for x in xrange(4):
+        tmp = yield x
+        if tmp == 'hello':
+            print 'world'
+        else:
+            print str(tmp)
+
+a = gen()
+print a.next()
+print
+print a.next()
