fb409bdb19ccbb304877d94365cf950a8c0bdd6e
gitttttt <zhangyifan@neotel.com.cn>
2016-12-14 11:12:57 +0800

    segment

--- _test.py
+++ _test.py
@@ -1,36 +1,29 @@
 class Solution(object):
-    def validIPAddress(self, IP):
+    def summaryRanges(self, nums):
         """
-        :type IP: str
-        :rtype: str
+        :type nums: List[int]
+        :rtype: List[str]
         """
-        if '.' in IP:
-            a1 = IP.split('.')
-            try:
-                if len(a1) == 4 and all(map(lambda x: self.part1(x), a1)):
-                    return 'IPv4'
-                else:
-                    return "Neither"
-            except:
-                return "Neither"
-        elif ':' in IP:
-            a2 = IP.split(':')
-            if len(a2) == 8 and all(map(lambda x: self.part2(x), a2)):
-                return 'IPv6'
+        if not nums:
+            return []
+        res = []
+        s = nums[0]
+        tmp = nums[0]
+        i = 1
+        while i < len(nums):
+            if nums[i] == tmp+1:
+                tmp = nums[i]
             else:
-                return "Neither"
+                if s == tmp:
+                    res.append(str(s))
+                else:
+                    res.append(str(s) + '->' + str(tmp))
+                s, tmp = nums[i], nums[i]
+            i += 1
+        if s == tmp:
+            res.append(str(s))
         else:
-            return "Neither"
+            res.append(str(s) + '->' + str(tmp))
+        return res
 
-    def part1(self, s):
-        if not all(map(lambda x: x.isdigit(), s)):
-            return False
-        if len(s) > 1 and s[0] == '0':
-            return False
-        return 0 <= int(s) <= 255
-
-    def part2(self, s):
-        alf = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F']
-        return 1 <= len(s) <= 4 and all(map(lambda x: x.isdigit() or x in alf , s))
-
-print Solution().validIPAddress("172.16.254.-1")
+print Solution().summaryRanges([1,2])
